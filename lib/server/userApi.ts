ce54d2c95acf674a43e9ce6e86db1ac0400b3e72
Johanna Herrmann <johanna.herrmann@mailbox.org>
2025-12-25 20:09:47 +0100

    feat: adds user request handling and user api

---
+++ lib/server/userApi.ts
@@ -0,0 +1,31 @@
+import express, { Express } from 'express';
+import {
+  addUserHandler,
+  deleteUserHandler,
+  getUsersHandler,
+  loginHandler,
+  modifyCredentialsHandler, modifyDisplayNameHandler,
+  modifyOwnCredentialsHandler,
+  modifyOwnUsernameHandler,
+  modifyPermissionsHandler,
+  modifyUsernameHandler,
+  userManagerHandler
+} from '@/server/handler/userHandler';
+
+const buildUserApi = function (): Express {
+  const api = express();
+  api.use('/manage', userManagerHandler);
+  api.post('/manage/add', addUserHandler);
+  api.post('/manage/modify-username', modifyUsernameHandler);
+  api.post('/modify-username', modifyOwnUsernameHandler);
+  api.post('/manage/modify-credentials', modifyCredentialsHandler);
+  api.post('/modify-credentials', modifyOwnCredentialsHandler);
+  api.post('/manage/modify-permissions', modifyPermissionsHandler);
+  api.post('/manage/modify-displayname', modifyDisplayNameHandler);
+  api.delete('/manage/delete/:userId', deleteUserHandler);
+  api.get('/manage/get-users', getUsersHandler);
+  api.post('/login', loginHandler);
+  return api;
+};
+
+export { buildUserApi };
