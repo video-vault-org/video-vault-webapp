951a53e880eddf113594aae0510c3761339e58ef
Johanna Herrmann <j.herrmann@neusta.de>
2026-01-13 15:29:23 +0100

    feat: improves test setup for windows by using happy-dom instead of jsdom

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
     }
   },
   test: {
-    environment: 'jsdom',
+    environment: 'happy-dom',
     globals: true,
     setupFiles: './src/test/setup.ts',
     css: true,
